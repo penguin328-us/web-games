a4e60e96b83e93a593e27c9fa71557ab4c2ade9d
penguin328 <zliyong@gmail.com>
2017-03-08 02:41:48 +0000

    bundle socket.io

--- views/common/gameClientBase.js
+++ views/common/gameClientBase.js
@@ -4,12 +4,12 @@ const Person = require("../../common/person.js");
 const ChatClient = require("../../common/services/chat/chatClient.js");
 const RoomClient = require("../../common/services/room/roomClient.js");
 const RoleClient = require("../../common/services/role/roleClient.js");
+const io = require("socket.io-client");
 const user = require("./user");
 
 
 module.exports = class GameClientBase {
     constructor(namespace, connectedCallback) {
-        /* global io */
         const socket = io(namespace).connect();
         socket.on("connect", (() => {
             this.person = new Person(user.getDisplayName(), socket);
